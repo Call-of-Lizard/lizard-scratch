0d49cd4cd7a53d078feb55882a5a81928b86ac39
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-09-22 14:07:37 -0700

    Wheaty buttons
    The buttons are wheaty

--- lizardScratch.js
+++ lizardScratch.js
@@ -1,5 +1,11 @@
 function fromEnglish() {
     var useThisLater = "lol"
-    console.log(document.getElementById('inputAreaEnglish').value)
+    console.log("English input:" + document.getElementById('inputAreaEnglish').value)
+    return useThisLater
+}
+
+function fromScratch() {
+    var useThisLater = "lol"
+    console.log("Scratch input:" + document.getElementById('inputAreaEnglish').value)
     return useThisLater
 }
