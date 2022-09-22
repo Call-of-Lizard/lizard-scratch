bb6a6bee44991df5f532c3c51905cb289711716e
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-09-22 14:37:34 -0700

    Update lizardScratch.js

--- lizardScratch.js
+++ lizardScratch.js
@@ -1,11 +1,28 @@
 function fromEnglish() {
-    var useThisLater = "lol"
-    console.log("English input:" + document.getElementById('inputAreaEnglish').value)
-    return useThisLater
+    //setup
+    var leString = "No input";
+    var leStringOutput = "";
+    var i;
+    console.log("English input:" + document.getElementById('inputAreaEnglish').value) //debug
+    leString = document.getElementById('inputAreaEnglish').value;
+
+    //basic scramble
+    for(let i = 0; i < leString.length; i++){
+        //turn to number
+    console.log(leString.charCodeAt(i) - 69);
+    console.log("loop: " + i);
+        //add to string
+    leStringOutput = leStringOutput + Number(leString.charCodeAt(i) - 69);
+    }
+    
+    console.log(leStringOutput)
+
+    //end
+    return leString
 }
 
 function fromScratch() {
     var useThisLater = "lol"
-    console.log("Scratch input:" + document.getElementById('inputAreaEnglish').value)
+    console.log("Scratch input:" + document.getElementById('inputAreaEnglish').value) //debug
     return useThisLater
 }
